462a69d665f5b8f66216afc15470fa10e0ae94ef
Ravin Kumar <ravinsdrive@gmail.com>
2020-07-30 20:02:33 -0700

    Remove old fields

--- code/exercises/1_say_hi.py
+++
@@ -1,4 +0,0 @@
-# Using your knowledge of for loops, lists, and string concatenations
-# write a program where you say hi to each person in this training
-
-names = []
--- code/exercises/3_get_all_releases.py
+++
@@ -1,8 +0,0 @@
-# Using a python get the last release date and version of multiple python proects
-
-
-# Hint: Go onto pypi and find the xml for pandas, xarray, pymc3, or whatever
-# other libraries you want
-project_xmls = []
-
-# Fill in the code to parse the xml and print the versions
